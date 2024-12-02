0631fd60cd7f4b5aad209860a2cb6c95a8f3c985
Fatmear <2079303425@qq.com>
2024-12-02 16:01:03 +0800

    add fgm842d csdk

---
+++ ql_components/api/ql_aes.h
@@ -0,0 +1,13 @@
+/*================================================================
+  Copyright (c) 2021, Quectel Wireless Solutions Co., Ltd. All rights reserved.
+  Quectel Wireless Solutions Proprietary and Confidential.
+=================================================================*/
+#ifndef QL_AES_H
+#define  QL_AES_H
+
+
+extern INT32  ql_aes_string_encryption( char *szin, INT32 inlen,  UINT8 *key,  UINT8 *ciphertext);
+
+#endif
+
+
---
+++ ql_components/api/ql_web.h
@@ -0,0 +1,9 @@
+#ifndef QL_WEB_H
+#define QL_WEB_H
+
+
+
+int ql_web_on(void);
+
+int ql_web_off(void);
+#endif
